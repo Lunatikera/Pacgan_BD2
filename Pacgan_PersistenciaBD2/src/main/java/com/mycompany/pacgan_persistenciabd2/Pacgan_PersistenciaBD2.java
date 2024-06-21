cafbb201a30fdc6c065ead6243185b876d297426
ReneEzequiel23 <150867485+ReneEzequiel23@users.noreply.github.com>
2024-06-20 20:56:31 -0700

    commit

--- Pacgan_PersistenciaBD2/src/main/java/com/mycompany/pacgan_persistenciabd2/Pacgan_PersistenciaBD2.java
+++ Pacgan_PersistenciaBD2/src/main/java/com/mycompany/pacgan_persistenciabd2/Pacgan_PersistenciaBD2.java
@@ -5,13 +5,21 @@
 
 package com.mycompany.pacgan_persistenciabd2;
 
+import daos.BeneficiarioDAO;
+import entidades.BeneficiarioEntidad;
+import excepciones.PersistenciaException;
+
+
 /**
  *
  * @author Usuario
  */
 public class Pacgan_PersistenciaBD2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PersistenciaException {
         System.out.println("Hello World!");
+        BeneficiarioDAO bene = new BeneficiarioDAO();
+        BeneficiarioEntidad b = new BeneficiarioEntidad();
+        bene.agregarBeneficiario(b);
     }
 }
--- Pacgan_PersistenciaBD2/target/generated-sources/annotations/entidades/BeneficiarioEntidad_.java
+++ Pacgan_PersistenciaBD2/target/generated-sources/annotations/entidades/BeneficiarioEntidad_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-20T20:09:45", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-20T19:21:16", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(BeneficiarioEntidad.class)
 public class BeneficiarioEntidad_ { 
 
