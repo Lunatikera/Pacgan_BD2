64f6c0c4ccb18af4c426eb93f21799187df3b95e
Lunatikera <carlitosjuven@gmail.com>
2024-06-23 06:08:55 -0600

    Push (Capa Negocio)
    capa negocio y configuracion tablas de presentacion

--- Pacgan_PersistenciaBD2/target/generated-sources/annotations/entidades/AbonoEntidad_.java
+++ Pacgan_PersistenciaBD2/target/generated-sources/annotations/entidades/AbonoEntidad_.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-23T06:27:06", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-23T07:07:31", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(AbonoEntidad.class)
 public class AbonoEntidad_ { 
 
