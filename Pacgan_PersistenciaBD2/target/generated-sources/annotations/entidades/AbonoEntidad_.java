3c64d0c0d7ae53f9409081dba448942b47f06809
Lunatikera <carlitosjuven@gmail.com>
2024-06-26 23:08:03 -0700

    push

--- Pacgan_PersistenciaBD2/target/generated-sources/annotations/entidades/AbonoEntidad_.java
+++ Pacgan_PersistenciaBD2/target/generated-sources/annotations/entidades/AbonoEntidad_.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-27T00:31:49", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-26T22:52:14", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(AbonoEntidad.class)
 public class AbonoEntidad_ { 
 
